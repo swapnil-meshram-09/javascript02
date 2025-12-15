e72440d1ef8d560700905f689fcc5ba6ac4c94a9
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2025-12-15 14:05:28 +0530

    new commit

---
+++ prompt.js
@@ -0,0 +1,36 @@
+
+console.log("hello");
+
+
+
+let num1 = 123;
+
+console.log("This is a number:",num1);               // one space before 123
+
+
+
+
+let num3 = 456;
+
+console.log("This is a number:"+num1);              // no space before 123
+
+
+
+
+let num2 = 789;
+
+console.log(`This is a number:${num2}`);           // no space before 123
+
+
+
+
+let str1 = "java";
+
+let str2 = "script";
+
+console.log(str1+str2);
+
+
+
+
+
